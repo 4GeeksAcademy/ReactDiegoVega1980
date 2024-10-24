f0b8fdf2c2be4197e3d20a1a5c7d8bf740fad903
DVG-Dexter <161875360+DVG-Dexter@users.noreply.github.com>
2024-10-24 06:48:02 +0000

    ok version

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 
 
-const Footer=() =>{
-    return(
-            
-          <div className="footer bg-dark text-white" style={{ position: "fixed", bottom: 100, width: "100%" }}>
-
-            Diego Vega 4Geeks Academy
-
-          </div>
-    );
+const Footer = () => {
+  return (
+    <div className="footer bg-dark text-white w-100 mt-5">
+      Diego Vega 4Geeks Academy
+    </div>
+  );
 };
 
-export  default Footer
+export default Footer
